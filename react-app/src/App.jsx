d953f3d77706a2a3990abf24c386a6d87d7a76e0
smammc <smammc@hotmail.com>
2024-07-30 18:05:46 +0100

    First day - missing footer

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import menuIcon from "./assets/menu-icon.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <nav
+        className="navbar"
+        style={{ width: document.body.offsetWidth, top: 0 }}
+      >
+        <img className="menu" src={menuIcon} alt="menu-icon" />
+      </nav>
+      <div className="sidebar">
+        <ul>
+          <li>Homepage</li>
+          <li>Apartments</li>
+          <li>About Us</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <footer></footer>
+    </div>
+  );
 }
-
-export default App
+export default App;
